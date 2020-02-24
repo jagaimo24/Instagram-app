8199d980d244d86cd7a4dfbb8e07456469a8be78
machitani <d.dhm0824@gmail.com>
2020-02-24 19:58:13 +0900

    carrier_wave.rbの修正

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,13 +1,13 @@
 if Rails.env.production?
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: 'AWS',
-    region: 'ap-northeast-1'
-    aws_access_key_id: 'AKIA2EMMD6ROMIV42G7N',
-    aws_secret_access_key: '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',
-  }
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider: 'AWS',
+      region: 'ap-northeast-1',
+      aws_access_key_id: 'AKIA2EMMD6ROMIV42G7N',
+      aws_secret_access_key: 'LqoyS5Yl+QYFvaFtF5fIGyd8JPS+NHBBjKK4nOKI'
+    }
 
-  config.fog_directory  = 'rails-insta-sample'
-  config.cache_storage = :fog
-end
+    config.fog_directory  = 'rails-insta-sample'
+    config.cache_storage = :fog
+  end
 end
